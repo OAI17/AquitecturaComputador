c6ca635c88fa873771905e55949af4a5fcfd8080
Tomas's Home <tomasmarmay21@gmail.com>
2023-09-11 20:22:05 -0300

    p1 y p2 terminados

--- practico2/signext.sv
+++ practico2/signext.sv
@@ -5,7 +5,7 @@ module signext
 			always_comb
 				casez (a[31:21])
 					11 'b111_1100_00?0: y = { {55{a[20]}}, a[20:12]}; // D instruccion
-					11 'b101_1010_0???: y = { {43{a[20]}}, a[23:5], 2'b0  }; // CB instruccion
+					11 'b101_1010_????: y = { {45{a[23]}}, a[23:5]}; // CB instruccion , 2'b0  
 					default y = '0;
 				endcase
 endmodule 
